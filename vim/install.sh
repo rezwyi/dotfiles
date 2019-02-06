bc372f3c6251bdf1d4104c330f96ee9439a0b69b
Sergey Rezvanov <sergey@rezvanov.info>
2019-02-06 14:32:36 +0500

    Install additional vim plugins, update vim config

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-mkdir $HOME/.vim/
+set -e
+
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
+
+curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+if [ ! -d $HOME/.vim/bundle/nerdtree ]; then
+  git clone --quiet https://github.com/scrooloose/nerdtree.git $HOME/.vim/bundle/nerdtree
+fi
+
 cp -R dist/ $HOME/.vim/
 cp config $HOME/.vimrc
 
