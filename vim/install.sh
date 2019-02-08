0d87a6657eeb10979f9056ba84494c7ca2afc9a7
Sergey Rezvanov <sergey@rezvanov.info>
2019-02-08 09:59:09 +0500

    Add some perfomance commands to .vimrc

--- vim/install.sh
+++ vim/install.sh
@@ -5,6 +5,7 @@ set -e
 mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
 
 curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 if [ ! -d $HOME/.vim/bundle/nerdtree ]; then
   git clone --quiet https://github.com/scrooloose/nerdtree.git $HOME/.vim/bundle/nerdtree
 fi
