ac0aaa6e9db219b7de64d56146723fed74e457a1
Sergey Rezvanov <sergey@rezvanov.info>
2019-05-14 18:22:33 +0500

    Update options removed from tmux

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,7 +3,11 @@
 set -e
 
 mkdir -p $HOME/.tmux && cd $HOME/.tmux
-git clone https://github.com/seebi/tmux-colors-solarized.git
+
+if [ ! -d $HOME/.tmux/tmux-colors-solarized ]; then
+  git clone --single-branch --branch updateOptionsRemovedFromTmux --quiet https://github.com/darcyparker/tmux-colors-solarized.git
+fi
+
 cd -
 cp config $HOME/.tmux.conf
 
